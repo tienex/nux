1762991bed17714baff815587cdea2dfae1ce2bb
Gianluca Guida <glguida@tlbflush.org>
2019-10-26 16:23:53 +0100

    HAL/i386: Fix AP booting

--- libhal_x86/i386/i386_hal.c
+++ libhal_x86/i386/i386_hal.c
@@ -1,4 +1,5 @@
 #include <cdefs.h>
+#include <assert.h>
 #include <inttypes.h>
 #include <stdbool.h>
 #include <string.h>
@@ -8,20 +9,37 @@
 #include "i386.h"
 #include "../internal.h"
 
+static pfn_t pcpu_stackpfn[MAXCPUS];
+static void *pcpu_stackva[MAXCPUS];
+
 void
 hal_pcpu_init (unsigned pcpuid, struct hal_cpu *pcpu)
 {
-#if 0
+  pfn_t pfn;
+  void *va;
   void _set_tss (unsigned, void *);
   void _set_fs (unsigned, void *);
 
-  pcpu->tss.esp0 = KERNBASE + KERN_APBOOT(pcpuid) + 4096;
+  assert (pcpuid < MAXCPUS);
+
+  /* Allocate PCPU stack. Use KVA. */
+  pfn = pfn_alloc (1);
+  assert (pfn != PFN_INVALID);
+  /* This is tricky. The hope is that is low enough to be addressed by 16 bit. */
+  assert (pfn < (1 << 8) && "Can't allocate Memory below 1MB!");
+
+  va = kva_map (1, pfn, 1, HAL_PTE_W|HAL_PTE_P);
+  assert (va != NULL);
+
+  pcpu_stackpfn[pcpuid] = pfn;
+  pcpu_stackva[pcpuid] = va;
+
+  pcpu->tss.esp0 = pcpu_stackva[pcpuid] + 4096;
   pcpu->tss.ss0 = KDS;
   pcpu->tss.iomap = 108;
   pcpu->data = NULL;
   _set_tss (pcpuid, &pcpu->tss);
   _set_fs (pcpuid, &pcpu->data);
-#endif
 }
 
 void
@@ -30,6 +48,8 @@ hal_pcpu_enter (unsigned pcpuid)
   uint16_t tss = (5 + 4 * pcpuid) << 3;
   uint16_t fs = (5 + 4 * pcpuid + 1) << 3;
 
+  assert (pcpuid < MAXCPUS);
+
   asm volatile ("ltr %%ax"::"a" (tss));
   asm volatile ("mov %%ax, %%fs"::"a" (fs));
 }
@@ -37,31 +57,37 @@ hal_pcpu_enter (unsigned pcpuid)
 uint64_t
 hal_pcpu_prepare (unsigned pcpu)
 {
-#if 0
-  extern char *_ap_start, *_ap_end;
-  paddr_t pstart = KERN_APBOOT (pcpu);
-  void *start = (void *) (KERNBASE + (uintptr_t) pstart);
-  volatile uint16_t *reset = (volatile uint16_t *) (KERNBASE + 0x467);
+  extern volatile long _ap_stackpage;
+  extern char *_ap_start, *_ap_end, _ap_cr3;
+  paddr_t pstart;
+  void *start;
+  volatile uint16_t *reset;
 
   if (pcpu >= MAXCPUS)
     return PADDR_INVALID;
 
+  if (pcpu_stackva[pcpu] == NULL)
+    return PADDR_INVALID;
+
+  pstart = pcpu_stackpfn[pcpu] << PAGE_SHIFT;
+  start = pcpu_stackva[pcpu];
+  printf ("preparing %p (%lx)\n", pstart, start);
+
+  reset = kva_physmap (0, 0x467, 2, HAL_PTE_P|HAL_PTE_W|HAL_PTE_X);
   *reset = pstart & 0xf;
   *(reset + 1) = pstart >> 4;
+  kva_unmap (reset);
 
   /* Setup AP bootstrap page */
   memcpy (start, &_ap_start,
 	  (size_t) ((void *) &_ap_end - (void *) &_ap_start));
 
+  asm volatile ("":::"memory");
+
+  /* Setup AP Stack. */
+  void *ptr = start + ((void *)&_ap_stackpage - (void *)&_ap_start);
+  *(uintptr_t *)ptr = start + PAGE_SIZE;
   return pstart;
-#endif
-#warning Resolve the KERN_APBOOT issue.
-  /* 
-     We should keep one page reserved for AP boot that becomes stack, then
-     we allocate a proper page for the AP stack.
-     Then we can init another CPU.
-  */
-  return 0;
 }
 
 void
