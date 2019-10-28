81d94ea2145614c45473efb69d068effdbe4fd30
Gianluca Guida <glguida@tlbflush.org>
2019-10-28 01:04:41 +0000

    mk: set per-arch builddir

--- mk/top.mk
+++ mk/top.mk
@@ -1,6 +1,13 @@
+#
+# NUX: A kernel Library.
+# Copyright (C) 2019 Gianluca Guida <glguida@tlbflush.org>
+#
+#  SPDX-License-Identifier:	GPL2.0+
+#
+
 .PHONY: all install clean
 CC=@CC@
-OBJDIR=.build
+OBJDIR=.build/@MACHINE@/
 SRCROOT=@top_srcdir@
 MKDIR=$(SRCROOT)/@mk_dir@
 INSTALLDIR=@INSTALLDIR@
