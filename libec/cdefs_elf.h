e5deaf262a6e9c679273e348db57368a83d27ea1
Gianluca Guida <glguida@tlbflush.org>
2024-08-15 22:09:34 +0100

    libec: redefine __weak_alias a-la netbsd

--- libec/cdefs_elf.h
+++ libec/cdefs_elf.h
@@ -9,15 +9,13 @@
 
 #include <machine/asm.h>
 
-#define __decl_weak_alias(alias,sym)				\
+#define __weak_alias(alias,sym)					\
   __asm(".weak " _C_LABEL_STRING(alias) "\n"			\
 	_C_LABEL_STRING(alias) " = " _C_LABEL_STRING(sym))
 
-#define __decl_alias(alias,sym)					\
+#define __alias(alias,sym)					\
   __asm(".globl " _C_LABEL_STRING(alias) "\n"			\
 	_C_LABEL_STRING(alias) " = " _C_LABEL_STRING(sym))
 
-#define __weak_alias(x) __attribute__((weak, alias (#x)))
-#define __alias(x) __attribute__((alias (#x)))
 
 #endif /* EC_CDEFS_ELF_H */
